d05570bd09b3a7e9a9597ad6067ca232a7313dec
amyoun01 <amyoun01@gmail.com>
2023-02-02 14:19:44 -0700

    Updated config file

--- wildfire_analysis/config.py
+++ wildfire_analysis/config.py
@@ -13,18 +13,18 @@ model_results_data_dir = data_dir / 'model_results'
 geo_lims = [-177.1875,35.0,-35.0,79.375]
 
 gcm_list = [
-    'ACCESS-CM2',
-    'CNRM-CM6-1-HR',
     'EC-Earth3-Veg',
     'MPI-ESM1-2-HR',
-    'MRI-ESM2-0'
+    'MRI-ESM2-0',
+    'CNRM-CM6-1-HR',
+    'ACCESS-CM2',
     ]
 
 metvars = [
     'tasmax',
     'pr',
     'sfcWind',
-    'hursmin'
+    'hursmin',
     ]
 
 fire_yr = [1950,2020]
@@ -34,20 +34,19 @@ cmip6_yr = [1979,2099]
 hst_yr = [1980,2009]
 sim_periods = ([2010,2039],[2040,2069],[2070,2099])
 
-
 # Values needed for quantile delta mapping
 oper = {
     'tasmax': '+',
     'pr': '*',
     'sfcWind': '*',
-    'hursmin': '*'
+    'hursmin': '*',
     }
 
 min_thresh = {
     'tasmax': None,
     'pr': 0.5,
     'sfcWind': None,
-    'hursmin': None
+    'hursmin': None,
     }
 
 quantile_vals = [0.005] + [x/100 for x in range(1,100)] + [0.995]
