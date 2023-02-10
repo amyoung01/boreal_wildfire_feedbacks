89baa05c06fac7892ac3e3703d910f67b4b7f2f7
amyoun01 <amyoun01@gmail.com>
2023-02-10 15:57:22 -0700

    Major modifications to download fire data workflow

--- bash/download_fire_data.sh
+++ bash/download_fire_data.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 ###############################################################################
-#
 # Initialize environment and variables
-#
 ###############################################################################
 
 # Activate conda environment
@@ -18,6 +16,11 @@ WDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Spatial reference system information
 PRJ=$WDIR/../data/ancillary/AK_Canada_aea.prj
 
+# Get fire year range from config.yaml using 'yq' package 
+# (https://github.com/mikefarah/yq)
+YR1=$(yq '.TIME.fire_yr[0]' $WDIR/../wildfire_analysis/config.yaml)
+YR2=$(yq '.TIME.fire_yr[1]' $WDIR/../wildfire_analysis/config.yaml)
+
 # Designate and create directories
 DATA_DIR=$WDIR/../data/raw/fire/
 DEST=$WDIR/../data/processed/fire/shapefiles
@@ -29,119 +32,155 @@ mkdir -p $CAN_DIR
 mkdir -p $DEST
 
 ###############################################################################
-#
 # Download fire datasets for Alaska and Canada
-#
 ###############################################################################
 
 # Download Alaska fire history
-cd $AK_DIR
-echo
-echo "Downloading Alaska fire polygons ..."
-echo
-curl -o ak_fire.zip https://fire.ak.blm.gov/content/maps/aicc/Data/Data%20%28zipped%20filegeodatabases%29/AlaskaFireHistory_Polygons_1940_2020.zip -L
-unzip ak_fire.zip
-echo " ... finished downloading Alaska fire datasets!"
+# cd $AK_DIR
+# echo
+# echo "Downloading Alaska fire polygons ..."
+# echo
+# curl -o ak_fire.zip https://fire.ak.blm.gov/content/maps/aicc/Data/Data%20%28zipped%20filegeodatabases%29/AlaskaFireHistory_Polygons_1940_2022.zip -L
+# unzip ak_fire.zip
+# echo " ... finished downloading Alaska fire datasets!"
 
-# Download Canada fire history
-cd $CAN_DIR
-echo
-echo "Downloading Canada fire polygons ..."
-echo
-curl -o can_fire.zip https://cwfis.cfs.nrcan.gc.ca/downloads/nfdb/fire_poly/current_version/NFDB_poly_large_fires.zip -L
-unzip can_fire.zip
-echo "finished downloading Canada fire datasets."
-
-# For Alaska, also need to convert from geodatabase (.gdb) to shapefile (.shp).
-# Also, for Alaska, limit to "large" fire events and remove all fire perimeters 
-# < 200 ha.
+# # Download Canada fire history
+# cd $CAN_DIR
+# echo
+# echo "Downloading Canada fire polygons ..."
+# echo
+# curl -o can_fire.zip https://cwfis.cfs.nrcan.gc.ca/downloads/nfdb/fire_poly/current_version/NFDB_poly_large_fires.zip -L
+# unzip can_fire.zip
+# echo "finished downloading Canada fire datasets."
 
 ###############################################################################
-#
 # Process AK fire history
 # -----------------------
-#
 # In the next steps for the Alaska fire dataset:
 #  1. Convert Alaska fire geodatabase to a shapefile and reproject to 
 #     predetermined spatial reference system
 #  2. Select all fire years between 1950-2020 and > 200 hectares (494.5 acres)
 #  3. Trim fields in shapefile to only FIREYR and ACRES
-#
 ###############################################################################
 
 echo
 echo "Using "$(gdalinfo --version)" for shapefile management ..."
 echo
 
+cd $AK_DIR
+
 # Get name of downloaded geodatabse
 GDB_NAME=$(basename -- *.gdb)
 
-# Set tmp files for writing intermediate data processing steps
-TMP1=tmp1.shp
-TMP2=tmp2.shp
-TMP3=tmp3.shp
-TMP4=tmp4.shp
-
 #  1. Convert Alaska fire geodatabase to a shapefile and reproject to 
 #  predetermined spatial reference system
+#
+#       '> /dev/null' prevents output from displaying to terminal
+#
+ogr2ogr -f "ESRI Shapefile" \
+  -t_srs $PRJ \
+  $DEST/tmp.shp $GDB_NAME > /dev/null
 
-ogr2ogr -t_srs $PRJ -f "ESRI Shapefile" $DEST/$TMP1 $GDB_NAME > /dev/null 2>&1
-
-# 2. Select all fire years between 1950-2020 and > 200 hectares (494.5 acres).
+# 2. Add column to attribute table signaling original data source (e.g., either
+# Alaska large fire database [AK_LFDB] or Canadian National Fire Database 
+# [CAN_NFDB]).
+ogrinfo $DEST/tmp.shp \
+  -sql "ALTER TABLE tmp ADD COLUMN DATASRC character(8)" > /dev/null
+ogrinfo $DEST/tmp.shp -dialect SQLite \
+  -sql "UPDATE tmp SET DATASRC = 'AK_LFDB'" > /dev/null
+ 
+# 3. Select all fire years between 1950-2020 and > 200 hectares .
 # First need to convert FIREYEAR field from string to integer and 
 # rename 'FIREYR'. 
-# 
-# '> /dev/null 2>&1' blocks output to terminal
+ogr2ogr -f "ESRI Shapefile" -overwrite \
+  -sql "SELECT *, CAST(FIREYEAR as integer) AS FIREYR FROM tmp" \
+   $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp' > /dev/null
 
+# Convert from ACRES -> HECTARES: HECTARES = 0.4047*ACRES
+ogr2ogr -f "ESRI Shapefile" -overwrite \
+  -sql "SELECT *, 0.4047*ACRES AS HECTARES, NAME AS FIRENAME FROM tmp" \
+   $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp' > /dev/null
+
+ogr2ogr -f "ESRI Shapefile" -overwrite \
+  -sql "SELECT * FROM tmp WHERE (FIREYR >= $YR1) AND \
+                                (FIREYR <= $YR2) AND \
+                                (HECTARES >= 200.0)" \
+  $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp' > /dev/null
+
+# 3. Trim fields (or attributes) in shapefile to only FIREYR and ACRES
 ogr2ogr -f "ESRI Shapefile" \
-  -sql "SELECT *, CAST(FIREYEAR as integer) AS FIREYR FROM tmp1" \
-   $DEST/$TMP2 $DEST/$TMP1 > /dev/null 2>&1
-
-ogr2ogr -f "ESRI Shapefile" \
-  -sql "SELECT * FROM tmp2 WHERE (FIREYR >= 1950) AND \
-                                 (FIREYR <= 2020) AND \
-                                 (ACRES > 494.5)" \
-  $DEST/$TMP3 $DEST/$TMP2 > /dev/null 2>&1
-
-# 3. Trim fields in shapefile to only FIREYR and ACRES
-
-ogr2ogr -f "ESRI Shapefile" \
-  -sql "SELECT FIREYR, ACRES FROM tmp3" \
-  $DEST/AK_LFDB_poly_large_fires.shp $DEST/$TMP3 > /dev/null 2>&1
+  -sql "SELECT DATASRC, FIRENAME, FIREYR, HECTARES FROM tmp" \
+  $DEST/AK_LFDB_poly_large_fires.shp $DEST/tmp.shp > /dev/null
 
 # Remove/delete temporary files
-
 rm $DEST/tmp*
 
+# ###############################################################################
+# # Process Canada fire history
+# # ---------------------------
+# # In the next steps for the Alaska fire dataset:
+# #  1. Reproject Canada fire shapefile to predetermined spatial reference system
+# #  2. Rename FIREYEAR to FIREYR to match AK shapefile
+# #  3. Convert SIZE_HA attribute to ACRES by multiplying by a factor of 2.471
+# #  4. Select all fire years between 1950-2020. Downloaded shapefile already
+# #     filters for large fires > 200 ha
+# #  3. Trim fields in shapefile to only FIREYR and ACRES
+# ###############################################################################
+
+cd $CAN_DIR
+
+# # Get name of downloaded shapefile
 SHP_NAME=$(basename -- *.shp)
 
-ogr2ogr -t_srs $PRJ -f "ESRI Shapefile" \
-  $DEST/$TMP1 $SHP_NAME > /dev/null 2>&1
-
+# 1. Reproject Canada fire shapefile
 ogr2ogr -f "ESRI Shapefile" \
-  -sql "SELECT *, YEAR AS FIREYR FROM tmp1" \
-  $DEST/$TMP2 $DEST/$TMP1 > /dev/null 2>&1
+  -t_srs $PRJ \
+  $DEST/tmp.shp $SHP_NAME > /dev/null
 
-# Convert HA to ACRES by multiplying by a factor of 2.471
-ogr2ogr -f "ESRI Shapefile" \
-  -sql "SELECT *, SIZE_HA*2.471 AS ACRES FROM tmp2" \
-  $DEST/$TMP3 $DEST/$TMP2 > /dev/null 2>&1
+# 2. Add column to attribute table signaling original data source (e.g., either
+# Alaska large fire database [AK_LFDB] or Canadian National Fire Database 
+# [CAN_NFDB]).
+ogrinfo $DEST/tmp.shp \
+  -sql "ALTER TABLE tmp ADD COLUMN DATASRC character(8)" > /dev/null
+ogrinfo $DEST/tmp.shp -dialect SQLite \
+  -sql "UPDATE tmp SET DATASRC = 'CAN_NFDB'" > /dev/null
+ 
+# 3. Select all fire years between 1950-2020 and > 200 hectares (494.5 acres).
+# First need to convert FIREYEAR field from string to integer and 
+# rename 'FIREYR'. 
+ogr2ogr -f "ESRI Shapefile" -overwrite \
+  -sql "SELECT *, YEAR AS FIREYR, SIZE_HA AS HECTARES FROM tmp" \
+   $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp' > /dev/null
 
-ogr2ogr -f "ESRI Shapefile" \
-  -sql "SELECT * FROM tmp3 WHERE (FIREYR >= 1950) AND (FIREYR <= 2020)" \
-  $DEST/$TMP4 $DEST/$TMP3 > /dev/null 2>&1
+ogr2ogr -f "ESRI Shapefile" -overwrite \
+  -sql "SELECT * FROM tmp WHERE (FIREYR >= $YR1) AND (FIREYR <= $YR2)" \
+  $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp' > /dev/null
 
+# 4. Trim fields (or attributes) in shapefile to DATASRC, FIRENAME, FIREYR and 
+# ACRES
 ogr2ogr -f "ESRI Shapefile" \
-  -sql "SELECT FIREYR, ACRES FROM tmp4" \
-  $DEST/CAN_NFDB_poly_large_fires.shp $DEST/$TMP4 > /dev/null 2>&1
+  -sql "SELECT DATASRC, FIRENAME, FIREYR, HECTARES FROM tmp" \
+  $DEST/CAN_NFDB_poly_large_fires.shp $DEST/tmp.shp > /dev/null
 
+# Remove/delete temporary files
+rm $DEST/tmp*
+
+# ##############################################################################
+# Merge AK and Canada fire history shapefiles into single shapefile and 
+# delete individual files for AK and Canada
+# ##############################################################################
+
+# First, create new shapefile with merged name using AK fire history
 ogr2ogr -f "ESRI Shapefile" \
-  $DEST/AK_Can_large_fire_history_1950-2020.shp \
-  $DEST/AK_LFDB_poly_large_fires.shp > /dev/null 2>&1
-  
+  $DEST/AK_Can_large_fire_history_$YR1-$YR2.shp \
+  $DEST/AK_LFDB_poly_large_fires.shp > /dev/null
+
+# Second, update merged shapefile by appending Canada fire history shapefile
 ogr2ogr -f "ESRI Shapefile" -update -append \
-  $DEST/AK_Can_large_fire_history_1950-2020.shp \
+  $DEST/AK_Can_large_fire_history_$YR1-$YR2.shp \
   $DEST/CAN_NFDB_poly_large_fires.shp \
-  -nln "AK_Can_large_fire_history_1950-2020" > /dev/null 2>&1
+  -nln "AK_Can_large_fire_history_$YR1-$YR2" > /dev/null
 
-rm $DEST/tmp* $DEST/AK_LFDB_poly_large_fires* $DEST/CAN_NFDB_poly_large_fires*
+# Finally, remove AK and Canada files
+rm $DEST/AK_LFDB_poly_large_fires* 
+rm $DEST/CAN_NFDB_poly_large_fires*
