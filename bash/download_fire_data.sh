0da968869e88137d8bdae2d0c9330b9dd0105149
amyoun01 <amyoun01@gmail.com>
2023-02-16 00:16:40 -0700

    Small changes to scripts to manage fire datasets

--- bash/download_fire_data.sh
+++ bash/download_fire_data.sh
@@ -36,22 +36,23 @@ mkdir -p $DEST
 ###############################################################################
 
 # Download Alaska fire history
-# cd $AK_DIR
-# echo
-# echo "Downloading Alaska fire polygons ..."
-# echo
-# curl -o ak_fire.zip https://fire.ak.blm.gov/content/maps/aicc/Data/Data%20%28zipped%20filegeodatabases%29/AlaskaFireHistory_Polygons_1940_2022.zip -L
-# unzip ak_fire.zip
-# echo " ... finished downloading Alaska fire datasets!"
+# TODO: add wildcard if geodatabase name changes in the future like _1940-2023.zip
+cd $AK_DIR
+echo
+echo "Downloading Alaska fire polygons ..."
+echo
+curl -o ak_fire.zip https://fire.ak.blm.gov/content/maps/aicc/Data/Data%20%28zipped%20filegeodatabases%29/AlaskaFireHistory_Polygons_1940_2022.zip -L
+unzip ak_fire.zip
+echo " ... finished downloading Alaska fire datasets!"
 
 # # Download Canada fire history
-# cd $CAN_DIR
-# echo
-# echo "Downloading Canada fire polygons ..."
-# echo
-# curl -o can_fire.zip https://cwfis.cfs.nrcan.gc.ca/downloads/nfdb/fire_poly/current_version/NFDB_poly_large_fires.zip -L
-# unzip can_fire.zip
-# echo "finished downloading Canada fire datasets."
+cd $CAN_DIR
+echo
+echo "Downloading Canada fire polygons ..."
+echo
+curl -o can_fire.zip https://cwfis.cfs.nrcan.gc.ca/downloads/nfdb/fire_poly/current_version/NFDB_poly_large_fires.zip -L
+unzip can_fire.zip
+echo "finished downloading Canada fire datasets."
 
 ###############################################################################
 # Process AK fire history
@@ -74,12 +75,10 @@ GDB_NAME=$(basename -- *.gdb)
 
 #  1. Convert Alaska fire geodatabase to a shapefile and reproject to 
 #  predetermined spatial reference system
-#
-#       '> /dev/null' prevents output from displaying to terminal
-#
+
 ogr2ogr -f "ESRI Shapefile" \
   -t_srs $PRJ \
-  $DEST/tmp.shp $GDB_NAME > /dev/null
+  $DEST/tmp.shp $GDB_NAME
 
 # 2. Add column to attribute table signaling original data source (e.g., either
 # Alaska large fire database [AK_LFDB] or Canadian National Fire Database 
@@ -87,30 +86,30 @@ ogr2ogr -f "ESRI Shapefile" \
 ogrinfo $DEST/tmp.shp \
   -sql "ALTER TABLE tmp ADD COLUMN DATASRC character(8)" > /dev/null
 ogrinfo $DEST/tmp.shp -dialect SQLite \
-  -sql "UPDATE tmp SET DATASRC = 'AK_LFDB'" > /dev/null
+  -sql "UPDATE tmp SET DATASRC = 'AK_LFDB'"
  
 # 3. Select all fire years between 1950-2020 and > 200 hectares .
 # First need to convert FIREYEAR field from string to integer and 
 # rename 'FIREYR'. 
 ogr2ogr -f "ESRI Shapefile" -overwrite \
   -sql "SELECT *, CAST(FIREYEAR as integer) AS FIREYR FROM tmp" \
-   $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp' > /dev/null
+   $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp'
 
 # Convert from ACRES -> HECTARES: HECTARES = 0.4047*ACRES
 ogr2ogr -f "ESRI Shapefile" -overwrite \
   -sql "SELECT *, 0.4047*ACRES AS HECTARES, NAME AS FIRENAME FROM tmp" \
-   $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp' > /dev/null
+   $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp'
 
 ogr2ogr -f "ESRI Shapefile" -overwrite \
   -sql "SELECT * FROM tmp WHERE (FIREYR >= $YR1) AND \
                                 (FIREYR <= $YR2) AND \
                                 (HECTARES >= 200.0)" \
-  $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp' > /dev/null
+  $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp'
 
 # 3. Trim fields (or attributes) in shapefile to only FIREYR and ACRES
 ogr2ogr -f "ESRI Shapefile" \
   -sql "SELECT DATASRC, FIRENAME, FIREYR, HECTARES FROM tmp" \
-  $DEST/AK_LFDB_poly_large_fires.shp $DEST/tmp.shp > /dev/null
+  $DEST/AK_LFDB_poly_large_fires.shp $DEST/tmp.shp
 
 # Remove/delete temporary files
 rm $DEST/tmp*
@@ -135,32 +134,32 @@ SHP_NAME=$(basename -- *.shp)
 # 1. Reproject Canada fire shapefile
 ogr2ogr -f "ESRI Shapefile" \
   -t_srs $PRJ \
-  $DEST/tmp.shp $SHP_NAME > /dev/null
+  $DEST/tmp.shp $SHP_NAME
 
 # 2. Add column to attribute table signaling original data source (e.g., either
 # Alaska large fire database [AK_LFDB] or Canadian National Fire Database 
 # [CAN_NFDB]).
 ogrinfo $DEST/tmp.shp \
-  -sql "ALTER TABLE tmp ADD COLUMN DATASRC character(8)" > /dev/null
+  -sql "ALTER TABLE tmp ADD COLUMN DATASRC character(8)"
 ogrinfo $DEST/tmp.shp -dialect SQLite \
-  -sql "UPDATE tmp SET DATASRC = 'CAN_NFDB'" > /dev/null
+  -sql "UPDATE tmp SET DATASRC = 'CAN_NFDB'"
  
 # 3. Select all fire years between 1950-2020 and > 200 hectares (494.5 acres).
 # First need to convert FIREYEAR field from string to integer and 
 # rename 'FIREYR'. 
 ogr2ogr -f "ESRI Shapefile" -overwrite \
   -sql "SELECT *, YEAR AS FIREYR, SIZE_HA AS HECTARES FROM tmp" \
-   $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp' > /dev/null
+   $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp'
 
 ogr2ogr -f "ESRI Shapefile" -overwrite \
   -sql "SELECT * FROM tmp WHERE (FIREYR >= $YR1) AND (FIREYR <= $YR2)" \
-  $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp' > /dev/null
+  $DEST/tmp.shp $DEST/tmp.shp -nln 'tmp'
 
 # 4. Trim fields (or attributes) in shapefile to DATASRC, FIRENAME, FIREYR and 
 # ACRES
 ogr2ogr -f "ESRI Shapefile" \
   -sql "SELECT DATASRC, FIRENAME, FIREYR, HECTARES FROM tmp" \
-  $DEST/CAN_NFDB_poly_large_fires.shp $DEST/tmp.shp > /dev/null
+  $DEST/CAN_NFDB_poly_large_fires.shp $DEST/tmp.shp
 
 # Remove/delete temporary files
 rm $DEST/tmp*
@@ -172,14 +171,14 @@ rm $DEST/tmp*
 
 # First, create new shapefile with merged name using AK fire history
 ogr2ogr -f "ESRI Shapefile" \
-  $DEST/AK_Can_large_fire_history_$YR1-$YR2.shp \
-  $DEST/AK_LFDB_poly_large_fires.shp > /dev/null
+  $DEST/AK_Canada_large_fire_history.shp \
+  $DEST/AK_LFDB_poly_large_fires.shp
 
 # Second, update merged shapefile by appending Canada fire history shapefile
 ogr2ogr -f "ESRI Shapefile" -update -append \
-  $DEST/AK_Can_large_fire_history_$YR1-$YR2.shp \
+  $DEST/AK_Canada_large_fire_history.shp \
   $DEST/CAN_NFDB_poly_large_fires.shp \
-  -nln "AK_Can_large_fire_history_$YR1-$YR2" > /dev/null
+  -nln "AK_Canada_large_fire_history"
 
 # Finally, remove AK and Canada files
 rm $DEST/AK_LFDB_poly_large_fires* 
