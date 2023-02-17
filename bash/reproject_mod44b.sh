e4d5cde78630858b3daa6874695c4c05888360ae
amyoun01 <amyoun01@gmail.com>
2023-02-17 00:18:12 -0700

    Initial commit script to reproject mod44b

---
+++ bash/reproject_mod44b.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+###############################################################################
+# Initialize environment and variables
+###############################################################################
+
+# Activate conda environment
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate py39
+
+# Set up directory names
+
+# Find directory of current file, source: https://stackoverflow.com/a/246128
+WDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DEST=$WDIR/../data/processed/veg/mod44b
+
+mkdir -p $DEST
+
+# Get fire year range from config.yaml using 'yq' package 
+# (https://github.com/mikefarah/yq)
+# Map extent (meters)
+EXTENT=( \
+  $(yq '.EXTENT.proj_lims[0]' $WDIR/../wildfire_analysis/config.yaml) \
+  $(yq '.EXTENT.proj_lims[1]' $WDIR/../wildfire_analysis/config.yaml) \
+  $(yq '.EXTENT.proj_lims[2]' $WDIR/../wildfire_analysis/config.yaml) \
+  $(yq '.EXTENT.proj_lims[3]' $WDIR/../wildfire_analysis/config.yaml) \
+)
+
+# CRS=$( yq '.CRS' $WDIR/../wildfire_analysis/config.yaml )
+# Spatial reference system information
+CRS=$WDIR/../data/ancillary/AK_Canada_aea.prj
+
+FILES=(
+      $( ls $WDIR/../data/raw/veg/mod44b/*Percent_Tree_Cover*.tif )
+      $( ls $WDIR/../data/raw/veg/mod44b/*Quality*.tif )
+      $( ls $WDIR/../data/raw/veg/mod44b/*Cloud*.tif )
+    )
+
+for f in ${FILES[@]}; do
+
+  gdalwarp \
+    -tr 250 250 \
+    -te ${EXTENT[@]} \
+    -t_srs $CRS \
+    -overwrite \
+    $f $DEST/$( basename "$f" )
+
+done
