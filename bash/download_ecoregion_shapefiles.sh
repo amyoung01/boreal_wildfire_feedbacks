c7d32b5be6f84421803cb840d0ca6a91b528b6ea
amyoun01 <amyoun01@gmail.com>
2023-03-16 08:22:41 -0700

    Modified ecoregion download script

--- bash/download_ecoregion_shapefiles.sh
+++ bash/download_ecoregion_shapefiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p data/raw/ecoregions
+
 curl -o tmp/official_teow.zip https://files.worldwildlife.org/wwfcmsprod/files/Publication/file/6kcchn7e3u_official_teow.zip?_ga=2.252261194.2115546270.1678845839-789025250.1678845839
 unzip tmp/official_teow.zip -d data/raw/ecoregions
 rm -f tmp/official_teow.zip
